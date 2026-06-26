e287ae6c8e04e45de08917b7bce02959adeb892a
Yogesh <yogeshmadhukumar86@gmail.com>
2026-06-26 12:19:50 +0530

    added PDF file upload input

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("AI Resume Analyser")
 st.write("Upload your resume and get feedback.")
+
+#adding file upload
+uploaded_file = st.file_uploader("Upload your resume PDF", type=["pdf"])
+if uploaded_file:
+    st.success(f"Loaded: {uploaded_file.name}")
